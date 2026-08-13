a8611426c8dc273544f04968f5c1d536dd8a6270
sjourdro <simon.jourdrouin@inria.fr>
2026-08-13 09:32:15 +0200

    add draw tutorials from viewer

---
+++ modules/python/examples/image/tutorial-draw-cross.py
@@ -0,0 +1,24 @@
+#https://visp-doc.inria.fr/doxygen/visp-daily/tutorial-draw-cross_8cpp_source.html
+
+from visp.core import ImageGray
+from visp.core import Display
+from visp.core import ImagePoint
+from visp.core import Color
+from visp.python.display_utils import get_display
+
+# Creating a gray image
+I = ImageGray(2160, 3840, 128)
+#I = ImageGray(800, 800, 128)
+d = get_display()
+d.setDownScalingFactor(Display.SCALE_AUTO)
+d.init(I)
+Display.setTitle(I, "Point drawing")
+
+# Drawing a red cross on the image
+Display.display(I)
+Display.displayCross(I, ImagePoint(I.getHeight()/2, I.getWidth()/2), int(I.getWidth()/2), Color.red, 2)
+Display.flush(I)
+
+# Waiting for user input
+print("A click to quit...")
+Display.getClick(I)
---
+++ modules/python/examples/image/tutorial-draw-text.py
@@ -0,0 +1,25 @@
+#https://visp-doc.inria.fr/doxygen/visp-daily/tutorial-draw-text_8cpp_source.html
+
+from visp.core import ImageGray
+from visp.core import Display
+from visp.core import ImagePoint
+from visp.core import Color
+from visp.python.display_utils import get_display
+
+# Creating a gray image
+I = ImageGray(2160, 3840, 128)
+#I = ImageGray(800, 800, 128)
+d = get_display()
+d.setDownScalingFactor(Display.SCALE_AUTO)
+d.init(I)
+Display.setTitle(I, "Point drawing")
+
+# Drawing a yellow text in a red rectangle on the image
+Display.display(I)
+Display.displayRectangle(I, ImagePoint(I.getHeight()/4, I.getWidth()/4), ImagePoint(I.getHeight()*3/4, I.getWidth()*3/4), Color.red, True)
+Display.displayText(I, ImagePoint(I.getHeight()/2, I.getWidth()/2), "Hello World!", Color.yellow)
+Display.flush(I)
+
+# Waiting for user input
+print("A click to quit...")
+Display.getClick(I)
